6a3e7b37fcc9137f7e4a4b0217ef11031951a77f
Michal-Placek <michal.placek@student.ossp.cz>
2021-12-02 13:53:29 +0100

    contact.php

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakt</title>
+</head>
+<body>
+
+    <h1>Kontakt</h1>
+
+     <p>
+        E-Mail: martin.kokes@ossp.cz
+        Telefon: 792308605
+        Adresa: Ovčárecká 240
+        FAX: 606872394
+     </p>
+
+</body>
+</html>

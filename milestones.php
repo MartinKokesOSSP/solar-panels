bf1c4a5971034cd619ff5c722d11bd49779d8aa7
NgoThanhHieu <thanhhieu.ngo@student.ossp.cz>
2021-12-02 13:56:55 +0100

    Úspěchy první verze

---
+++ milestones.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Úspěchy</title>
+</head>
+<body>
+
+<h1 style="color:gold">Naše Úspěchy</h1>
+<p style="color:red">Patříme mezi nejlepší firmy v oblasti energetiky po celém svetě</p>
+<table>
+    <tr>
+        <td><img src="https://img.ihned.cz/attachment.php/970/43035970/a2EPDbhQ4oF3ky6HMCdpBtvVOm8sgIAN/infografika_uspechy.png" width="720" alt=""></td>
+   <td><img src="https://thumbs.dreamstime.com/b/red-arrow-icon-directly-white-background-icon-red-arrow-direction-white-background-104072627.jpg" width="180" alt=""><img src="https://solenergy.com.ph/wp-content/uploads/2018/01/Achievements-towards-Solar-Energy-Philippines-in-Recent-Years.png" width="360" height="80" alt=""></td>     
+
+    </tr>
+</table>
+</body>
+</html>

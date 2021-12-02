1422235090c0a92f97afb4ba86d48ddf08fc2db3
SimonSiksta420 <simon.siksta@student.ossp.cz>
2021-12-02 13:42:05 +0100

    Create o-nas.php

---
+++ o-nas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O nás</title>
+</head>
+<body>
+
+    <h1>O nás</h1>
+
+     <p>
+      Naše firma prodává ty nejlepší solární panely na světě.
+      Prodáváme po celé České republice a plánujeme i exportovat i do zahraničí.
+      Povedlo se nám rekordně snížit emise v České republice na úplnou nulu !!!!!!
+      Podporujeme Evropský Green Deal a sponzorujeme jejich propagaci.
+      Jsme epický kupujte naše produkty jinak budeme velice smutní :(. 
+     </p>
+
+</body>
+</html>

cb286514d0e00907cc1288e97d4624ec806df49f
KarelNakladal <60643873+KarelNakladal@users.noreply.github.com>
2021-12-02 13:13:16 +0100

    basic catalog added

---
+++ catalog.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php">domů</a>
+<div class="catalog">
+    <table> 
+        <tr>
+            <th><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.4camping.cz%2Ffiles%2Fphotos%2F1600-1200%2F7%2F795ab34e-solarni-panel-gosun-solar-panel-30w.png&f=1&nofb=1" alt="" style="max-width:400px"></th>
+            <th><h1>Solar Pad 300</h1></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fw24cdn.cz%2Fwww.spyobchod.cz%2F_%2F1200x800-0-0-0-0%2Fproduct%2Fproduct_124662%2F96463e3a02ef7991bea896cff72ed924.jpg&f=1&nofb=1" alt="" style="max-width:400px"></th>
+            <th><h1>Sun Block 36</h1></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.kupindoslike.com%2FSolarni-panel-270W-24V-polikristalni_slika_XL_85652793.jpg&f=1&nofb=1" alt="" style="max-width:400px"></th>
+            <th><h1>Black Energy 4</h1></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.kupindoslike.com%2FSolarni-panel-20W_slika_O_99249635.jpg&f=1&nofb=1" alt="" style="max-width:400px"></th>
+            <th><h1>Green Energy HK</h1></th>
+            <th></th>
+        </tr>
+        
+    
+    </table>
+</div>
+    
+</body>
+</html>

e2cfbeb11e6b45d6cb1d6357daad21f3951e4ec9
JakubVavru <JakubVavru@users.noreply.github.com>
2021-12-02 13:29:45 +0100

    Created faq.php
    Nejčastěji pokládané otázky byly zodpovězeny :)

---
+++ faq.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FaQ</title>
+</head>
+<body>
+    <strong><a href="">Domů</a></strong> <strong><a href="">Produkty</a></strong> <strong><a href="">FaQ</a></strong> <strong><a href="">O nás</a></strong>
+    <h1>FaQ</h1>
+
+<h2>Kolik energie vyrobí fotovoltaický panel?</h2>
+1kWp nainstalovaného výkonu vyrobí za rok průměrně 900kWh elektrické energie. Tato hodnota může být vyšší až o 10% díky vyšší nadmořské výšce (nebo nižší, v nížinách bývají častěji mlhy a inverze), může však být nižší i díky nemožnosti umístit panely do optimální orientace. Vyrobený výkon se může lišit také v závislosti na geografickém umístění, statisticky nejvíce slunečních dnů je na Jižní Moravě a v Jižních Čechách, nejméně v Severních Čechách. Hodnota 900kWh/rok/1kWp je průměrná, závisí na počtu slunečních dnů v daném roce a to závisí na klimatických podmínkách, které jsou rok od roku různé. Do této hodnoty jsou započítány i ztráty na vodičích a měniči.
+
+<h2>Jaká je životnost fotovoltaických panelů?</h2>
+Většina výrobců udává životnost okolo 25 let. Záruční doba na panely a měniče je obvykle pět let, u měničů napětí se dá připlatit za prodlouženou záruku až na 7 až 20 let. Fotovoltaický panel však průběhu životnosti degraduje, výrobci obvykle garantují 90% účinnost po 12 letech a 80% účinnost panelu po 25 letech. Teoreticky lze provozovat fotovoltaický panel mnohem déle, například 30 let, otázkou je však výhodnost jeho provozování při neustále se snižující účinnosti a dále díky neustálému vývoji nových technologií může za 20 let (tedy po uplynutí doby garantované výkupní ceny) být výhodnější nakoupit nové, účinnější a levnější fotovoltaické panely.
+
+<h2>Jaký je optimální sklon a orientace panelů?</h2>
+Ideální orientace je přímo na jih, při orientaci v rozsahu jiho-východ až jiho-západ jsou maximální ztráty dosaženého výkonu přibližně 5%. Panely lze orientovat i vodorovně při ztrátě 10% nebo svisle při ztrátě 30%. Sklon panelů závisí na typu systému a způsobu jeho využívání, při celoročním provozu ostrovních systémů je lépe umístit panely více "nakolmo" (49°), protože sluneční kotouč je nízko v zimních měsících, naopak maximalizaci zisku u systémů pro výrobu elektřiny do sítě dosáhneme umístěním panelů více "naležato" (32°), protože během letních měsíců, kdy je nejvíce slunečních dnů a Slunce je vysoko na Zemi dopadá 75% ročního úhrnu globálního záření.
+
+<h2>Jak se vyrobená energie vykupuje a co je to zelený bonus?</h2>
+Fotovoltaický systém můžete mít na výrobu výhradně do sítě distributorské firmy, v takovém případě inkasujete výkupní cenu (13,46Kč/kWh bez DPH). Pokud část elektrické energie spotřebováváte, inkasujete zelený bonus (12,65Kč/kWh bez DPH) s tím, že spotřebovanou energii nemusíte nakupovat, čímž realizujete úsporu. Zelené bonusy lze inkasovat i za výrobu energie do akumulátorů u ostrovních systémů, vyrobená energie však musí být měřena. Zelený bonus je zkráceně "prémie za výrobu elektřiny čistým způsobem". Výkupní cena i zelený bonus jsou garantovány na 20 let od uvedení systému do provozu
+
+<h2>Bude distributorská společnost moji elektřinu vykupovat?</h2>
+Distributorské společnosti mají ze zákona (zák. 180/2005 Sb. o podpoře výroby elektřiny z OZ) povinnost přednostně připojit k distribuční síti zařízení vyrábějící elektřinu z obnovitelných zdrojů, pokud jsou splněny veškeré stanovené technické požadavky zařízení a mají povinnost veškerou vyrobenou elektřinu vykupovat.
+
+<h2>Jak probíhá zúčtování vyrobené elektřiny?</h2>
+Zúčtovacím obdobím je (pokud není dohodnuto jinak) kalendářní měsíc. Vyrobená energie je fakturována distribuční společnosti na základě pravidelně zasílaných naměřených údajů. Fakturovaná částka není během roku konstantní, v zimních měsících bývá vlivem klimatických podmínek útlum výroby, naopak největší zisky jsou dosahovány během optimálního počasí v letních měsících.
+
+<h2>Kolik stojí fotovoltaický systém?</h2>
+Cena fotovoltaických systémů se obvykle udává v ceně za instalovanou 1kW výkonu zařízení. Cena instalace se pohybuje okolo 135.000Kč/kWp. U cenových nabídek je nutné rozlišovat, co vše je součástí ceny - některé firmy do ceny instalace nezahrnují asistenci při získání licence na výrobu elektřiny, při získání smlouvy o připojení s distribuční společností a asistenci při podávání žádosti o dotaci.
+
+<h2>Za jak dlouho se fotovoltaický systém zaplatí?</h2>
+Doba návratnosti investice do fotovoltaického systému je přibližně 8 - 11 let bez dotace. Za předpokladu 20leté doby garance výkupní ceny a zeleného bonusu jde tedy o investici relativně výhodnou a bezpečnou vzhledem k tomu, že ERÚ výkupní ceny každoročně valorizuje o průmyslovou inflaci. Ekonomické parametry systému by měla výrazně vylepšit státní dotace, která u fotovoltaických zařízení do 5kWP činila v roce 2007 až 50% uznatelných nákladů, maximálně však 200.000Kč.
+
+<h2>Jaká se na fotovoltaické systémy vztahuje sazba DPH?</h2>
+U fotovoltaických instalcí na rodinných domech, bytových domech, panelových domech a podobně, u střešních nebo fasádních systémů lze uplatnit § 48 zákona o dani z přidané hodnoty, tedy sníženou 9% sazbu DPH jak na montážní práce, tak na samotné technické prostředky fotovoltaické elektrárny, protože se jedná o modernizaci v rámci bytové výstavby. Pokud je instalace umístěna mimo dům (na pozemku, budově nesloužící k bydlení, garáži apod.) jedná se o 19% sazbu DPH
+
+<h2>Jaká je lhůta od podání žádosti na SFŽP k proplacení této dotace?</h2>
+Došlé žádosti se posuzují čtvrtletně. Pokud žádost projde výběrovou komisí, je doba na vyplacení dotace cca 3 měsíce. Toto je standardní časový harmonogram pro vyplacení dotace, nicméně realita v roce 2007 byla 6 - 8 měsíců. Doba závisí na počtu podaných žádostí.
+
+<h2>Co znamená jednotka výkonu Wp (Watt peak)?</h2>
+Nominální výkon fotovoltaických panelů je udáván v jednotkách Watt peak (Wp), jde o výkon vyrobený solárním panelem při standardizovaném výkonnostním testu, tedy při energetické hustotě záření 1000W/m2, 25°C a světelném spektru odpovídajícím slunečnímu záření po průchodu bezoblačnou atmosférou Země (Air Mass 1,5). Watt peak je jednotkou špičkového výkonu dodávaného solárním zařízením za ideálních podmínek, jde tedy přibližně o výkon dodávaný panelem nebo systémem za běžného bezoblačného letního dne.
+
+<h2>Kolik metrů čtverečních je třeba na výkon 1kWp?</h2>
+1kWp nainstalovaného výkonu obnáší přibližně 8 metrů čtverečních panelů. Pokud jsou panely instalovány na volný vodorovný terén do řad za sebou, je zapotřebí plocha přibližně 2,6x větší, než je plocha samotných kolektorů, protože řady musí být za sebou v takoých rozestupech, aby si nestínily
+
+<h2>Výkupní cena pro fotovoltaiku na rok 2008?</h2>
+Dle cenového rozhodnutí ERÚ je výkupní cena elektřiny dodané do sítě stanovena na 13,46 Kč/kWh. Za Zelený bonus je cena elektřiny pro fotovoltaiku 12,65 Kč/kWh.
+
+<h2>Lze ručit v bance pozemkem, na kterém se plánuje výstavba PV elektrárny?</h2>
+Pozemek je možno použít jako záruku za úvěr, nemovitosti jsou bankami brány jako stabilní ručení.
+
+<h2>Jaký by měl být sled kroků při projektu?</h2>
+1. Myšleka,vize <br>
+2. Vypracování kvalitního podnikatelského záměru <br>
+3. Zajistit dostatečné finanční zajištění projektu <br>
+4. Výběr vhodné lokality <br>
+5. Výběr vhodného partnera pro dodávku a instalaci technologie <br>
+6. Správné načasování realizace projektu <br>
+7. Smluvní ošetření všech rizik při realizaci a následném provozu elektrárny<br>
+8. Kvalitní smluvní zajištění všech odběratelsko dodavatelských obchodních vztahů.
+
+</body>
+</html>

78c88458d125a435fdb84b1ef32741f9b20aa264
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2021-12-02 13:43:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>HOMEPAGE</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+</style>
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="aboutus.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ABOUT US</a>
+    <a href="catalog.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CATALOG</a>
+    <a href="contact.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
+    <a href="team.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">TEAM</a>
+    <a href="pricelist.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PRICELIST</a>
+    <a href="faq.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">FAQ</a>
+    <a href="gallery.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">GALLERY</a>
+    <a href="milestones.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">MILESTONES</a>
+    <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+  </div>
+</div>
+<div class="w3-content" style="max-width:2000px;margin-top:250px">
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:900px" id="band">
+    <h1 class="w3-wide">Vítejte na homepage solárních panelů</h1>
+</div>
+</body>
+</html>

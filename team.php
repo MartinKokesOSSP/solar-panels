fe443cf636471efd346d83ea373d533a1876c800
SorinEni <sorin.eni@student.ossp.cz>
2021-12-02 13:45:00 +0100

    team is done sir

---
+++ team.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h3>Naší pracovníci</h3>
+
+    <p>Michal Plaček - stránka s kontaktem (contact.php)</p>
+    <p>Karel Nakládal - katalog s obrázky solárních panelů (catalog.php)</p>
+    <p>Šimon Siksta - o nás - (aboutus.php)</p>
+    <p>Sorin Eni - náš tým (team.php)</p>
+    <p>Jan Šlechta - ceník pricelist.php</p>
+    <p>Jakub Vávrů - FAQ (faq.php)</p>
+    <p>Jakub Trejbal - galerie (gallery.php)</p>
+    <p>Ngo Hieu - úsěpchy (milestones.php)</p>
+
+
+
+
+</body>
+</html>
